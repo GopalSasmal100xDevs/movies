dcc71136eba2a25637cc236307fab904c9245f72
gopalprojectx100 <gopalsasmal100xdevs@gmail.com>
2024-09-27 09:44:55 +0530

    Testing

--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import { getAvarageDurationMovies } from "./8-avarage-duration-movies.js";
 function main(movies) {
   const ans = getAvarageDurationMovies(movies);
   console.log(ans);
+
+  console.log("This is for Testing purpose");
 }
 main(movies);
+
+
+
